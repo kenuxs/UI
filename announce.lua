915487127765c8245c2a05e49cc16b7f4e331774
kenuxs <144792959+kenuxs@users.noreply.github.com>
2024-05-22 11:55:50 +0200

    Update announce.lua

--- announce.lua
+++ announce.lua
@@ -1,1 +1,1 @@
-return "Fixxen soon ToggleUI Error"
+return "Fixxen soon ToggleUI Error, If you don't like the script, I don't care, I made it for myself and share it with you, I'll will ad soon autokil when I feel like it"
