ac07d0ba691d2967264a8ca47337775be440fae6
kenuxs <144792959+kenuxs@users.noreply.github.com>
2024-11-09 21:40:29 +0100

    Update announce.lua

--- announce.lua
+++ announce.lua
@@ -1,1 +1,1 @@
-return "Fixxen soon ToggleUI Error, If you don't like the script, I don't care, I made it for myself and share it with you, [Eat-Blobs-Simulator Still Worls !! (09.11.2024)]"
+return " If you don't like the script, I don't care, I made it for myself and share it with you, \n[Eat-Blobs-Simulator script Still Worls !! (09.11.2024)]"
